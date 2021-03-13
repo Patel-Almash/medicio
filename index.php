1765effc0469c01b865e1a7883189d3ab62d8328
Patel-Almash <67087714+Patel-Almash@users.noreply.github.com>
2021-03-13 11:03:29 +0530

    initial commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php include 'includes/header.php' ?>
+
+<main id="main">
+
+  <?php include 'pages/hero.php' ?> 
+  <?php include 'pages/featuredServices.php' ?> 
+  <?php include 'pages/cta.php' ?>  
+  <?php include 'pages/about.php' ?>  
+  <?php include 'pages/counts.php' ?>  
+  <?php include 'pages/features.php' ?>  
+  <?php include 'pages/services.php' ?>  
+  <?php include 'pages/appointment.php' ?>  
+  <?php include 'pages/departments.php' ?>  
+  <?php include 'pages/testimonials.php' ?> 
+  <?php include 'pages/doctors.php' ?>  
+  <?php include 'pages/gallery.php' ?>
+  <?php include 'pages/pricing.php' ?>
+  <?php include 'pages/faq.php' ?>
+  <?php include 'pages/contact.php' ?>
+
+</main><!-- End #main -->
+
+<?php include 'includes/footer.php' ?>
